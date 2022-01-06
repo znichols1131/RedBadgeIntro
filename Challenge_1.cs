3252f168aa63bd5f4e02af4bf56b99bab7641a37
Zach Nichols <znichols1131@gmail.com>
2022-01-05 19:08:29 -0500

    Challenge 3 alternate

--- Challenge_1.cs
+++ Challenge_1.cs
@@ -8,9 +8,37 @@ namespace RedBadgeIntroChallenges
 {
     public class Challenge_1
     {
-        public void Run()
+        public void Run_Zach()
         {
+            // Get list of strings
+            List<string> inputs = new List<string>() { "abc", "xyz", "klm", "xyz", "abc", "abc", "rst" };
 
+            // Set up output list
+            List<string> outputs = new List<string>();
+
+            // Filter using LINQ to remove not just duplicates but their pairs (i.e abc, abc, xyz => xyz)
+            outputs = inputs.GroupBy(i => i)
+                            .Where(o => o.Count() == 1)
+                            .Select(o => o.Key)
+                            .ToList();
+
+            // Output
+            Console.Write("Inputs: ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            foreach(string input in inputs)
+            {
+                Console.WriteLine(input);
+            }
+
+            Console.WriteLine("\nOutputs: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (string output in outputs)
+            {
+                Console.WriteLine(output);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadLine();
         }
     }
 }
