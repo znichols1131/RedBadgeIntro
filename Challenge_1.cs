5dd9416c82c7458076171226eda30d19bdb30d1e
Derek Jamerson <derekjamerson@gmail.com>
2022-01-05 19:07:50 -0500

    copied zachs styling

--- Challenge_1.cs
+++ Challenge_1.cs
@@ -8,9 +8,16 @@ namespace RedBadgeIntroChallenges
 {
     public class Challenge_1
     {
-        public void Run(List<string> input)
+        public void Run()
         {
-            List<string> result = input
+            // Get user input
+            Console.WriteLine("Enter any string for challenge 3: ");
+            string input = Console.ReadLine();
+
+            // Split string by spaces
+            string[] inputs = input.Trim().Split(' ');
+
+            List<string> result = inputs
                 .GroupBy(x => x)
                 .Where(x => x.Count() == 1)
                 .SelectMany(x => x)
