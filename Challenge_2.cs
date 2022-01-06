5473195e60474c4e8b429ca97d1627926518f559
Colin Dunn <dunncolinj@gmail.com>
2022-01-05 19:10:37 -0500

    Challenge #2

--- Challenge_2.cs
+++ Challenge_2.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace RedBadgeIntroChallenges
 {
     public class Challenge_2
     {
-        public void Run()
+        public bool LettersOnly(string input)
         {
-
+            Regex regex1 = new Regex("[A-Z][a-z]");
+            Regex regex2 = new Regex(".$");
+            return (regex1.IsMatch(input) && regex2.IsMatch(input));
         }
     }
 }
